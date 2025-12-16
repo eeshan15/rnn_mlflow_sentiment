6933a42885c2b5b10c858062a827d2c7011446dd
Eeshan <wanteddranzer2003@gmail.com>
2025-12-16 05:42:09 +0000

    Project Finalization

--- src/get_data.py
+++ src/get_data.py
@@ -7,9 +7,9 @@ data = {
     "text": [
         "amazing movie great story", "excellent performance loved it", "best film ever", "very good watch",
         "terrible movie waste of time", "bad acting poor script", "worst film ever", "boring and slow",
-        "loved the acting", "plot was terrible", "great cast but bad story", "waste of money"
+        "loved the acting", "plot was terrible", "great cast but bad story", "waste of money","Not worth it slept whole the time"
     ],
-    "label": [1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0]
+    "label": [1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0 ,0]
 }
 
 df = pd.DataFrame(data)
